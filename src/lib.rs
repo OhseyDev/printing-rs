0776a214a56f30e390aa0985d0b8b96b125bcb05
Ohsey <admin@ohsey.me>
2023-12-12 18:29:23 +0000

    Begin project

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[cfg(feature = "printify")]
+pub mod printify;
+#[cfg(feature = "printful")]
+pub mod printful;
+#[cfg(feature = "printaura")]
+pub mod printaura;
+#[cfg(feature = "spreadshirt")]
+pub mod spreadshirt;
+#[cfg(feature = "teaspring")]
+pub mod teaspring;
+
